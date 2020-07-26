1f35085f60f6d084f4cba161ad02755df51515fa
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-07-26 12:19:52 +0100

    Update syncBot.js

--- syncBot.js
+++ syncBot.js
@@ -6,6 +6,14 @@ client.on('ready', () => {
     client.user.setActivity("syncing roles", {type: "PLAYING"})
 })
 
-const { bot_token } = require("./config.js")
-
+const {bot_token} = require("./config.js")
 client.login(bot_token)
+
+const {role1} = require("./config.js")
+const {role2} = require("./config.js")
+if (member.roles(role => role1)){
+    roles.add('role2')
+}
+if (member.roles(role => role2)){
+    roles.add('role1')
+}
