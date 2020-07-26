5465ff23221ccf5009dbc3d45f284a7098c23282
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-07-26 15:00:40 +0100

    Update syncBot.js

--- syncBot.js
+++ syncBot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    console.log("Connected!" + client.user.tag)
     client.user.setActivity("syncing roles", {type: "PLAYING"})
 })
 
