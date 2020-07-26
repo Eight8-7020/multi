4d6ab892b9b2850ea55305d7b8e3bf001d3f7b5a
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-07-26 11:27:32 +0100

    Create syncBot-pm2.js

---
+++ syncBot-pm2.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: "syncBot",
+    script: "./syncBot.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "undefined",
+    }
+  }]
+}
