acc97ebdb3ed076468b8e1a170012c51f5348f5e
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-08-01 21:11:36 +0100

    Update syncBot.js

--- syncBot.js
+++ syncBot.js
@@ -2,28 +2,23 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+client.login(config.token)
 client.on('ready', () => {
     console.log("Connected!" + client.user.tag)
     client.user.setActivity("syncing roles", {type: "PLAYING"})
 })
 
-const {bot_token} = require("./config.js")
-client.login(bot_token)
+let role1 = message.guild.roles.cache.find(config.role1);
+let role2 = message.guild.roles.cache.find(config.role2);
 
-module.export.roles.role1 = { role1 } 
-module.export.roles.role2 = { role2 }
-
-let role1 = message.guild.roles.cache.find("role1");
-let role2 = message.guild.roles.cache.find("role2");
-
-if (message.guild.roles.cache.find(role1) {
-        if (message.member.roles.cache.some(role1)) {
-            message.member.roles.add(role2).catch(console.error);
+if (message.guild.roles.cache.find(config.role1) {
+        if (message.member.roles.cache.some(config.role1)) {
+            message.member.roles.add(config.role2).catch(console.error);
 
         }
 
-if (message.guild.roles.cache.find(role2) {
-        if (message.member.roles.cache.some(role2)) {
-            message.member.roles.add(role1).catch(console.error);
+if (message.guild.roles.cache.find(config.role2) {
+        if (message.member.roles.cache.some(config.role2)) {
+            message.member.roles.add(config.role1).catch(console.error);
 
         }
