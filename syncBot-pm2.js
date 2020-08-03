a37aaeec544bbfc2e14e7fd3334d2830abb89264
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-08-03 14:26:21 +0100

    Update syncBot-pm2.js

--- syncBot-pm2.js
+++ syncBot-pm2.js
@@ -1,12 +1,12 @@
 module.exports = {
-  apps : [{
-    name: "syncBot",
-    script: "~/syncBot.js",
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "undefined",
-    }
-  }]
+  apps : [
+      {
+        name: "syncBot",
+        script: "./syncBot.js",
+        watch: true,
+        env: {
+          "NODE_ENV": "development",
+        }
+      }
+  ]
 }
