cd11bdf8b102a9034a3dc16ea10c888735264a42
LFCfan25 <66164881+LFCfan25@users.noreply.github.com>
2020-08-16 17:40:29 +0100

    Update syncBot.js

--- syncBot.js
+++ syncBot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const fs = require('fs');
 const ini = require('ini');
 const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-const config = require("./config.json");
 
 client.login(config.token)
 client.on('ready', () => {
@@ -14,11 +13,9 @@ client.on('ready', () => {
 if (message.guild.roles.cache.find(config.role1) {
         if (message.member.roles.cache.some(config.role1)) {
             message.member.roles.add(config.role2).catch(console.error);
-
-        }
+}
 
 if (message.guild.roles.cache.find(config.role2) {
         if (message.member.roles.cache.some(config.role2)) {
             message.member.roles.add(config.role1).catch(console.error);
-
-        }
+}
