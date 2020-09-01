e0872dee1da89904c507dbb80b504bfa1898c826
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-09-01 11:43:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require('fs');
 
-if (config.ini.exists()) {
-    const fs = require('fs');
-    const ini = require('ini');
-    const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
-    };
-
-if (config.env.exists()) {
-    require('dotenv').config();
+if (fs.existsSync(./config.env)) {
+        require('dotenv').config();
     const config = require(process.env)
-    };
+    } else {if (fs.existsSync(./config.json)) {
+        const config = require(./config.json)
+        } else {if (fs.existsSync(./config.js)) {
+            var config = require('./config.js')
+            } else {if (fs.existsSync(./config.ini)){
+                const ini = require('ini');
+                const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
+                } else {@echo Please check that you have a config file.}
+                   };
 
 client.login(config.token)
 client.on('ready', () => {
     console.log("Connected!" + client.user.tag)
-    client.user.setActivity("myself sync roles", {type: "WATCHING"})
+    client.user.setActivity("config.statusmessage", {type: "config.statustype"})
 })
 
 if (message.guild.roles.cache.find(config.role1) {
